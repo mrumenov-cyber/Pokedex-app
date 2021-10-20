6b10d6f58ac9af8e5c07d5fd06b2fd71604023f0
mrumenov-cyber <marija.rumenovic@fesb.hr>
2021-10-20 21:08:25 +0200

    Exercise 2 - Add list of pokemons in js
    Added list of Pokemons with pokemons specifics in js

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Pokedex-js-app
  Application that displays a list of Pokemon and allows the user to view details for each of them
+
+ # TECH tools used
+ - JavaScript
+ - HTML
+ - CSS
+
+ 
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,4 +1,11 @@
-alert('Hello world!');
+let pokemonList = [
+    {name:'Bulbasaur', height:0.7, type: ['grass','poison']},
+    {name:'Charmender', height:0.6, type: ['fire']},
+    {name:'Pikachu', height:0.4, type: ['electric']},
+    {name:'Squirtle', height:0.5, type: ['water']},
+    {name:'Chikorita', height:0.9, type: ['grass']},
+    {name:'Swellow', height:0.7, type: ['Flying', 'Normal']},
+    {name:'Chikorita', height:0.5, type: ['fire']}
+    ];
 
-var favouriteFood = 'Cherry';
-document.write(favouriteFood);
+console.log(pokemonList);
