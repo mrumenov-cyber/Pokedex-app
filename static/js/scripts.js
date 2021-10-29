3b6b5201a004e9abe86ad06d0a17f44ae3fbdd30
mrumenov-cyber <marija.rumenovic@fesb.hr>
2021-10-29 10:18:18 +0200

    Exercise 6
    Added 2 new functions
    - addItemList()
    - showDetails()
    
    CSS
    -modifications
    -screen fits for smaller screens
    
    App is now passing directly to html elements (buttons) information from pokemonList - for now just pokemon.name
    
    Show Details function is on click function that displays all the information about clicked pokemon - ON CONSOLE

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,10 +1,11 @@
 // A new pokemonRepository variable to hold what your IIFE will return, 
 // then assigned IIFE to that variable
-let pokemonRepository = (function(){
+let pokemonRepository = (function
+    (){
     let pokemonList = [
         {name:'Bulbasaur', height: 0.7, types: ['grass','poison']},
         {name:'Charmender', height: 0.6, types: ['fire']},
-        {name:'Pikachu', height: 0.4, types: ['electric']},
+        {name:'Pikachu', height: 0.5, types: ['electric']},
         {name:'Squirtle', height: 0.5, types: ['water']},
         {name:'Chikorita', height: 0.9, types: ['grass']},
         {name:'Swellow', height: 0.7, types: ['Flying', 'Normal']},
@@ -12,19 +13,47 @@ let pokemonRepository = (function(){
         ];
 
         /*Function should add the Pokemon to the pokemonList array*/
-        function add(pokemon) {
+        function add(pokemon) { 
+            if (typeof pokemon === "object" &&
+            "name" in pokemon &&
+            "height" in pokemon &&
+            "types" in pokemon
+          ) {
             pokemonList.push(pokemon);
+          } else {
+            console.log("pokemon is not correct");
           }
+        }
     
        /*Function should return the pokemonList array*/
         function getAll() {
             return pokemonList;
           }
 
+          function showDetails(pokemon){
+              console.log(pokemon);
+          }
+
+   //Function that is reading html elements and then passing list of pokemons to each button
+   //Event listener is passing more information about each pokemon we click, but on console for now
+          function addListItem(pokemon){
+            let pokemonList = document.querySelector(".pokemon-list");
+            let listpokemon = document.createElement("li");
+            let button = document.createElement("button");
+            button.innerText = pokemon.name;
+            button.classList.add("button-style");
+            listpokemon.appendChild(button);
+            button.addEventListener('click', function () {
+                showDetails(pokemon);
+            });
+            pokemonList.appendChild(listpokemon);
+          }
+
       /*Return all the above functions and values, inside the IIFE, to the outside */
           return {
             add: add,
             getAll: getAll,
+            addListItem: addListItem
           };
 //End of wrapped PokemonList in IIFE function          
 })();
@@ -32,43 +61,7 @@ let pokemonRepository = (function(){
 // I am catching the whole Pokemon List from IIFE protected function, to be able to print it out
 pokemonList = pokemonRepository.getAll();
 
-// PRINTING OUT THE LIST OF POKEMONS IN THE LIST WITH *** FOR ***
-/*for (i=0; i<pokemonList.length; i++)
-{   
-    text = "Name of the Pokemon: " + pokemonList[i].name + " - Height: " + pokemonList[i].height + "m " + " - Types: " + pokemonList[i].types;
-    document.write(text);
-    // Condition to determine the biggest pokemon and to compare it with my results from up, and then to print it out
-    if (bigPokemon == pokemonList[i].height){
-     document.write("  - Wow, thats big! This is the one of the biggest pokemon out here." + "<br>");
-    }
-    else {document.write("<br>");
-    }
-}*/
-
-// I am determining the biggest pokemon of all, 
-// if there are two with the same biggest size, all of them will be printed later on
-    let bigPokemon = null;
-            for (k=0; k<pokemonList.length; k++)
-            {
-                if(bigPokemon <= pokemonList[k].height)
-                { 
-                    bigPokemon = pokemonList[k].height;
-                    k++;
-                } else {
-                    k++;
-                }
-            }
-    // PRINTING OUT THE LIST OF POKEMONS IN THE LIST WITH *** forEach() LOOP ****
-    function myLoopFunction(pokemon) {
-        let text=" ";
-        text = "Name of the Pokemon: " + pokemon.name + " - Height: " + pokemon.height + "m " + " - Types: " + pokemon.types;
-        document.write(text);
-    // Condition to determine the biggest pokemon and to compare it with my results from up, and then to print it out
-        if (bigPokemon == pokemon.height){
-        document.write("  - Wow, thats big! This is the one of the biggest pokemon out here." + "<br>");
-        }
-        else {document.write("<br>");
-        }
-    }
-
-  pokemonList.forEach(myLoopFunction);
+//Calling function allListItem() to execute the code on our page, and passing forEach to move through list of pokemons
+pokemonRepository.getAll().forEach(function (pokemon) {
+    pokemonRepository.addListItem(pokemon);
+  });
